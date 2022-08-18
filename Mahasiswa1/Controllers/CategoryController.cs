c60bbe601d7ec8acbb44174eb2f2a34b61f0b8d7
unknown <abanglevi@gmail.com>
2022-08-18 16:33:21 +0700

    file baru

---
+++ Mahasiswa1/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Mahasiswa1.Data;
+using Mahasiswa1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mahasiswa1.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ Mahasiswa1/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mahasiswa1.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nama {get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
